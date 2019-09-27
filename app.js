f8e008745505a96e60cb8aae83e91f069389c0fb
vysakh <vysakhprakash88@gmail.com>
2019-09-27 18:10:45 +0530

    change in port

--- app.js
+++ app.js
@@ -165,10 +165,15 @@ app.post("/", function(req, res) {
 
 
 
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
 
 
 
-app.listen(process.env.PORT || 3000, function() {
+
+app.listen (port, function() {
   console.log("server has started at 3000");
 
 });
